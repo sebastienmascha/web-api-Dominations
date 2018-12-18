245c1b39c321357d50899c16f20c209ec33d5b82
Tristan <tristan.ghodsy@isep.fr>
2018-12-18 10:12:37 +0100

    Suppresion projet ext
    Le projet externe se trouve maintenant uniquement dans sa branche

--- PROJETEXT/Kingdomino/src/App.java
+++
@@ -1,6 +0,0 @@
-public class App {
-    public static void main(String[] args) {
-        Plateau p = new Plateau(); //* Création du model
-        Controleur c = new Controleur(p); //* Création du controleur
-    }
-}
